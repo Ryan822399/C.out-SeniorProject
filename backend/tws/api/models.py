bc9d034de5355e0b9763b9f95121d0429c93a409
andreology <37252145+andreology@users.noreply.github.com>
2020-01-28 10:53:44 -0800

    adding models for user Workout and Rating

--- backend/tws/api/models.py
+++ backend/tws/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Workout(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+class Rating(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1),
+     MaxValueValidator(5)])
+     #unique and index together prevents rating from the user on workout plan
+    class Meta:
+        unique_together = (('user', 'workout'),)
+        index_together = (('user', 'workout'),)
