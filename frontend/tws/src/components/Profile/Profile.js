1904638d5c19c59f320edc96ba971fd68682f977
Diaz-48 <44877038+Diaz-48@users.noreply.github.com>
2020-02-08 23:33:15 -0800

    Minor UI adjustments to the profile page.

--- frontend/tws/src/components/Profile/Profile.js
+++ frontend/tws/src/components/Profile/Profile.js
@@ -17,26 +17,31 @@ class Profile extends Component {
     return (
 
       <div>
-        <Navbar className="navigation" bg="light" expand="lg">
-          <Navbar.Brand href="#home">TWS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Profile</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
+        <body>
+          <Navbar className="navigation" bg="light" expand="lg">
+            <Navbar.Brand id="top" href="http://localhost:3000/profile">TWS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="http://localhost:3000/profile">Profile</Nav.Link>
+                <Nav.Link href="#Friends">Friends</Nav.Link>
+                <Nav.Link href="#Graphs">Graphs</Nav.Link>
+                <Nav.Link href="#Workout">Workout</Nav.Link>
+                <Nav.Link href="#Timeline">Timeline</Nav.Link>
+                <Nav.Link href="#PublicForum">Public Forum</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#top">Top</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#posts">Posts</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+        </body>
 
         <Media>
           <img
@@ -47,20 +52,23 @@ class Profile extends Component {
             alt="Joker Peter Griffin"
           />
           <Media.Body>
-            <h5>
+            <h4>
               Peter Griffin (Joker)
+            </h4>
+            <h5>
+              @PeterGriffin
             </h5>
             <p>
               Peter Griffin is the main character of the American animated sitcom Family Guy. He is voiced by the series' creator, Seth MacFarlane, and first appeared on television, along with the rest of the Griffin family, in the 15-minute pilot pitch of Family Guy on December 20, 1998. Peter was created and designed by MacFarlane himself. MacFarlane was asked to pitch a pilot to the Fox Broadcasting Company based on Larry & Steve, a short made by MacFarlane which featured a middle-aged character named Larry and an intellectual dog, Steve. After the pilot was given the green light, the Griffin family appeared in the episode "Death Has a Shadow".
             </p>
           </Media.Body>
         </Media>
+
         <h3>
           Posts
         </h3>
-
-        <CardGroup>
-          <Card>
+        <CardGroup id="posts">
+          <Card style={{background: "#222"}}>
             <Card.Img variant="top" src="https://i.ytimg.com/vi/eV5fUPU7zIU/maxresdefault.jpg" />
             <Card.Body>
               <Card.Title>Forcing My Wife To Eat</Card.Title>
@@ -69,31 +77,31 @@ class Profile extends Component {
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Posted 2 days ago</small>
             </Card.Footer>
           </Card>
-          <Card>
+          <Card style={{background: "#222"}}>
             <Card.Img variant="top" src="https://www.nydailynews.com/resizer/n4XDpTyDGkvQpEQle3t1lwIlUaQ=/415x233/top/www.trbimg.com/img-5c3ca7a6/turbine/ny-1547478934-8c63008drk-snap-image" />
             <Card.Body>
               <Card.Title>Peter Griffin vs. Donald Trump</Card.Title>
               <Card.Text>
-                What can I say? I showed him the hands.
+                He got salty.
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Posted 4 days ago</small>
             </Card.Footer>
           </Card>
-          <Card>
-            <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSflIQPy04nkBdQf-LAPsDmvhiKruzqPiHX-IwCjEUHxZHw4wNZ" />
+          <Card style={{background: "#222"}}>
+            <Card.Img variant="top" src="https://cdn1.thr.com/sites/default/files/imagecache/768x433/2016/05/family_guy_trump_emmy_campaign_0.jpg" />
             <Card.Body>
-              <Card.Title>Peter The Hedgehog</Card.Title>
+              <Card.Title>DJ</Card.Title>
               <Card.Text>
-                #GottaGoFast
+                #NoCaptionNeeded
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Posted 5 days ago</small>
             </Card.Footer>
           </Card>
         </CardGroup>
