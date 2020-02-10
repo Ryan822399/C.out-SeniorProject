6cbc783710c93b1c6c690925ec1b967637340107
Ryan822399 <ryan.c.hennes@gmail.com>
2020-02-09 23:35:59 -0800

    fixed the server not starting

--- backend/tws/api/urls.py
+++ backend/tws/api/urls.py
@@ -1,21 +1,15 @@
 from rest_framework import routers
 from .views import *
-from django.urls import include
-from django import settings
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path
+from django.conf.urls import include
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 router.register('workouts', WorkoutViewSet)
 router.register('ratings', RatingViewSet)
-router.register('posts', PostsViewSet)
+
 
 
 urlpatterns = [
 path('', include(router.urls)),
-path('', include('posts.urls')),
 ]
-
-if settings.DEBUG: # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
