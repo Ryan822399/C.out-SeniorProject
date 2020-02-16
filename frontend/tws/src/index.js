7a9eeb8d892fb217fb227f270355f5481ab5e1ce
andreology <37252145+andreology@users.noreply.github.com>
2020-02-16 14:48:31 -0800

    adding pages to containers

--- frontend/tws/src/index.js
+++ frontend/tws/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter} from 'react-router-dom';
-import Login from './components/Login/Login';
-import Profile from './components/Profile/Profile';
-import Friends from './components/Friends/Friends';
+import Login from './containers/Login/Login';
+import Profile from './containers/Profile/Profile';
+import Friends from './containers/Friends/Friends';
 import { CookiesProvider } from 'react-cookie';
-import HomePage from './components/HomePage/HomePage';
-import Graphs from './components/Graphs/Graphs';
+import HomePage from './containers/HomePage/HomePage';
+import Graphs from './containers/Graphs/Graphs';
 //<Route exact path="/workouts" component={App}/>
 const routing = (
   <BrowserRouter>
