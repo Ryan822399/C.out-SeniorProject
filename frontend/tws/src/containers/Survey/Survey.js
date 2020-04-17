f76586913eb41fce2fe339be7516c5b76f240738
Diaz-48 <44877038+Diaz-48@users.noreply.github.com>
2020-04-16 23:34:01 -0700

    Working on register section

--- frontend/tws/src/containers/Survey/Survey.js
+++ frontend/tws/src/containers/Survey/Survey.js
@@ -13,7 +13,20 @@ export default class Survey extends Component {
     card1: true,
     card2: false,
     card3: false,
-    card4: false
+    card4: false,
+    attributes: {
+      userName: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      bio: "",
+      heightFeet: 0,
+      heightInches: 0,
+      dob: "",
+      location: "",
+      picture: "",
+      accountType: "private"
+    }
   }
 
   update = () =>
@@ -36,12 +49,63 @@ export default class Survey extends Component {
       return progress
   }
 
-  form = () => {
+  inputChanged = event => {
+    let att = this.state.attributes;
+    att[event.target.name] = event.target.value;
+    this.setState({attributes: att});
+  }
+
+  handleImageChange = event => {
+    console.log(event.target.files[0]);
+    let att = this.state.attributes;
+    att[event.target.name] = event.target.files[0];
+    this.setState({attributes: att});
+  };
+
+  transition = val => () => {
+    if(val == "next") {
+      if(this.state.card1) {
+        this.setState({card1: false})
+        this.setState({card2: true})
+      }
+      else if(this.state.card2) {
+        this.setState({card2: false})
+        this.setState({card3: true})
+      }
+      else if(this.state.card3) {
+        this.setState({card3: false})
+        this.setState({card4: true})
+      }
+      else {
+        this.setState({card4: false})
+      }
+    }
+    else {
+      if(this.state.card2) {
+        this.setState({card2: false})
+        this.setState({card1: true})
+      }
+      else if(this.state.card3) {
+        this.setState({card3: false})
+        this.setState({card2: true})
+      }
+      else {
+        this.setState({card4: false})
+        this.setState({card3: true})
+      }
+    }
+  }
+
+  form1 = () => {
     return (
       <Form>
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="userName" placeholder="Enter Username" />
+          <Form.Control type="userName" placeholder="Enter Username" value={this.state.userName} onChange={this.inputChanged}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email" onChange={this.inputChanged}/>
         </Form.Group>
         <Row>
           <Col sm={{ size: 'auto', offset: 1 }}>
@@ -50,7 +114,7 @@ export default class Survey extends Component {
             </p>
           </Col>
           <Col sm={{ size: 'auto', offset: 1 }}>
-          <p>
+          <p  onClick={this.transition("next")}>
             Next
           </p>
           </Col>
@@ -59,14 +123,148 @@ export default class Survey extends Component {
     )
   }
 
-  card = () => {
+  form2 = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicFirstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="firstName" placeholder="Enter First Name" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lastName" placeholder="Enter Last Name" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <p onClick={this.transition("back")}>
+              Back
+            </p>
+          </Col>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+          <p onClick={this.transition("next")}>
+            Next
+          </p>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+
+  form3 = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicBio">
+          <Form.Label>Bio</Form.Label>
+          <Form.Control type="bio" placeholder="Enter Bio" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicLocation">
+          <Form.Label>Location</Form.Label>
+          <Form.Control type="location" placeholder="Enter Location" onChange={this.inputChanged}/>
+        </Form.Group>
+        <input type="file"
+                 id="picture"
+                 name="picture"
+                 accept="image/png, image/jpeg, image/JPG" onChange={this.handleImageChange}/>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <p onClick={this.transition("back")}>
+              Back
+            </p>
+          </Col>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+          <p onClick={this.transition("next")}>
+            Next
+          </p>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+
+  form4 = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicDOB">
+          <Form.Label>Date of Birth</Form.Label>
+          <Form.Control type="DOB" placeholder="Enter Date of Birth" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicHeightF">
+          <Form.Label>Height Feet</Form.Label>
+          <Form.Control type="heightF" placeholder="Enter Height (Feet)" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicHeightI">
+          <Form.Label>Height Inches</Form.Label>
+          <Form.Control type="heightI" placeholder="Enter Height (Inches)" onChange={this.inputChanged}/>
+        </Form.Group>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <p onClick={this.transition("back")}>
+              Back
+            </p>
+          </Col>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+          <p onClick={this.transition("next")}>
+            Next
+          </p>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+
+  card1 = () => {
+    return (
+      <div style={{ paddingTop: '30px' }}>
+      <Card>
+        <Card.Body>
+          <Card.Title>What Should You Go By?</Card.Title>
+          <Card.Text>
+            <this.form1 />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+    )
+  }
+
+  card2 = () => {
+    return (
+      <div style={{ paddingTop: '30px' }}>
+      <Card>
+        <Card.Body>
+          <Card.Title>Please Enter Your Name</Card.Title>
+          <Card.Text>
+            <this.form2 />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+    )
+  }
+
+  card3 = () => {
     return (
       <div style={{ paddingTop: '30px' }}>
       <Card>
         <Card.Body>
           <Card.Title>What is your Username?</Card.Title>
           <Card.Text>
-            <this.form />
+            <this.form3 />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+    )
+  }
+
+  card4 = () => {
+    return (
+      <div style={{ paddingTop: '30px' }}>
+      <Card>
+        <Card.Body>
+          <Card.Title>What is your Username?</Card.Title>
+          <Card.Text>
+            <this.form4 />
           </Card.Text>
         </Card.Body>
       </Card>
@@ -75,19 +273,31 @@ export default class Survey extends Component {
   }
   render () {
 
-    let content = <this.card />
+    let content;
+    if(this.state.card1) {
+        content = <this.card1 />;
+    }
+    else if(this.state.card2) {
+        content = <this.card2 />;
+    }
+    else if(this.state.card3) {
+        content = <this.card3 />;
+    }
+    else {
+        content = <this.card4 />;
+    }
 
     return (
       <div>
       <NavBar/>
-          <Row>
-            <Col>
-              <this.card />
-            </Col>
-            <Col>
-                  {this.update()}
-            </Col>
-          </Row>
+      <Row>
+        <Col>
+          {content}
+        </Col>
+        <Col>
+          {this.update()}
+        </Col>
+      </Row>
       </div>
     );
   }
