91eecb3cb247a69dd593d71ad1788f1921d31482
Diaz-48 <44877038+Diaz-48@users.noreply.github.com>
2020-04-16 21:17:58 -0700

    Register stuff

--- frontend/tws/src/containers/Survey/Survey.js
+++ frontend/tws/src/containers/Survey/Survey.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 import DonutChart from '../../components/Visuals/DonutChart/DonutChartWrapper';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Card, Form, Button} from 'react-bootstrap';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default class Survey extends Component {
@@ -21,7 +21,7 @@ export default class Survey extends Component {
               endAngle: this.state.x * (2*Math.PI)
           }})
           progress = <DonutChart status={this.state.status}/>
-          
+
           this.state.x += .1
 
         if(this.state.x >= 1) {
@@ -30,6 +30,42 @@ export default class Survey extends Component {
       }, 10000);
       return progress
   }
+
+  form = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="userName" placeholder="Enter Username" />
+        </Form.Group>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <Button variant="primary" type="back">
+              Back
+            </Button>
+          </Col>
+          <Col sm={{ size: 'auto', offset: 1 }}>
+            <Button variant="primary" type="next">
+              Next
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+
+  card = () => {
+    return (
+      <Card style={{ width: '' }}>
+        <Card.Body>
+          <Card.Title>What is your Username?</Card.Title>
+          <Card.Text>
+            <this.form />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  }
   render () {
 
 
@@ -37,14 +73,18 @@ export default class Survey extends Component {
     return (
       <div>
       <NavBar/>
-      <Row>
-      <Col>
-        <h3>heelo</h3>
-      </Col>
-      <Col>
-            {this.update()}
-      </Col>
-      </Row>
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col>
+              <this.card />
+            </Col>
+            <Col>
+                  {this.update()}
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
       </div>
     );
   }
